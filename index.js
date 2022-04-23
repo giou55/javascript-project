37f5fc94c34c8a6660b4ad7670de7edfb5124107
giou55 <giourmetakis00@gmail.com>
2022-04-23 16:30:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const username = 'Michael';
+
+const person = {
+    firstname: username,
+};
+
+console.log(person);
+
+const sayHello = (name) => {
+    console.log(`Hello ${name}`);
+};
+
+console.log(sayHello('Maria'));
+
+console.log('End');
